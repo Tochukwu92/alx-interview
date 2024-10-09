024a9795b413f88b5b09e1f61db730913f072392
Tochukwu92 <Ilonubatochukwu@gmail.com>
2024-10-09 22:21:11 +0100

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,6 +7,9 @@ def pascal_triangle(n):
     '''
     triangle = []
     
+    if n <= 0:
+        return triangle
+    
     for row in range(n):
         new_row = [1] #  initialize every row with 1
         if triangle: #  check if the triangle is not empty
@@ -16,3 +19,29 @@ def pascal_triangle(n):
             new_row.append(1)
         triangle.append(new_row)
     return triangle
+
+"""
+
+def pascal_triangle(n):
+    '''
+    calculate and output a pascal triangle
+    '''
+    #  declear list to hold the entire triangle 
+    triangle = []
+    for i in range(n):
+        
+        #  stores indivitual rows of the triangle
+        row = []
+        
+        #  loop through the rows
+        for j in range(i + 1):
+            
+            #  this stores first and second rows
+            if (j == 0) or (j == i):
+                row.append(1)
+            else:
+                row.append(
+                    triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(row)
+    return triangle  
+"""
